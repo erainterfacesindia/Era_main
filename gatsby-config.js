72b8241ed66c3d1e0fa75d7710e674ab64c0489c
Uday <uday@Udays-MacBook-Air.local>
2021-09-23 01:33:38 +0530

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,13 +37,13 @@ module.exports = {
         path: `${__dirname}/src/pages`,
       },
   },
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `posts`,
-      path: `${__dirname}/src/posts`,
-    },
-},
+//   {
+//     resolve: `gatsby-source-filesystem`,
+//     options: {
+//       name: `posts`,
+//       path: `${__dirname}/src/posts`,
+//     },
+// },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
